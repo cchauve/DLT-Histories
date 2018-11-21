e848f5488e80aa24c40602b32b9c4715bdec18ff
Yann Ponty <yann.ponty@lix.polytechnique.fr>
2018-11-21 17:40:45 +0100

    Make CSV beautiful again

--- src/stats.py
+++ src/stats.py
@@ -1,6 +1,7 @@
 import os,sys
 import random
 
+
 GGD_PREAMBLE = """TYPE=GRAMMAR
 
 SYMBOLS=WORDS
@@ -38,32 +39,46 @@ def createGGDGrammar(k,CancelDups=True,CancelLosses=True):
     return out
 
 class Node:
-    def __init__(self, children=[], id=-1):
+    def __init__(self, children=[], id=-1, time=-1):
         self.children = children[:]
         for c in self.children:
             c.setParent(self)
         self.id = id
         self.parent = None
-    def setParent(self,u):
-        self.parent = u
-    def getParent(self):
-        return self.parent
-    def isRoot(self):
-        return(self.parent==None)
-    def getChildren(self):
-        return self.children
+        self.time = time
     def getID(self):
         return self.id
     def setID(self,id):
         self.id =  id
+    def getTime(self):
+        return self.time
+
+    def setParent(self,u):
+        self.parent = u
+    def getParent(self):
+        return self.parent
+
+    def isRoot(self):
+        return(self.parent==None)
+    def isBinary(self):
+        return len(self.children)==2
+    def isUnary(self):
+        return len(self.children)==1
+    def isLeaf(self):
+        return len(self.children)==0
+
+    def getChildren(self):
+        return self.children
     def getLeft(self):
         assert len(self.children)==2
         return self.children[0]
     def getRight(self):
         assert len(self.children)==2
         return self.children[1]
-    def isLeaf(self):
-        return len(self.children)==0
+    def getChild(self):
+        assert len(self.children)==1
+        return self.children[0]
+
     def allNodes(self):
         res = []
         for c in self.children:
@@ -80,6 +95,8 @@ class Node:
                 res += c.allNodes()
             res += p.getIncomparable()
         return res
+    def getContemporary(self):
+        return [v for v in self.getIncomparable() if v.getTime() == self.time]
 
 def Ext(u):
     return 1.
@@ -97,19 +114,20 @@ def LossLbl(u):
 def DupLbl(u):
     return "D%s"%(u.getID())
 def TranLbl(u,v):
-    return "T%s-%s"%(u.getID(),v.getID())
+    return "T%s->%s"%(u.getID(),v.getID())
 
 ##def ExtLbl(u):
-##    return ""
-def LossLbl(u):
-    return ""
-def DupLbl(u):
-    return "D"
-def TranLbl(u,v):
-    return "T"
+##    return "Z"
+##def LossLbl(u):
+##    return "L"
+##def DupLbl(u):
+##    return "D"
+##def TranLbl(u,v):
+##    return "T%s"%(u.getTime())
 
 
 def fillMatrices(tree,N):
+    global ALLOW_TRANSFERS
     nodes = tree.allNodes()
     S = [[0 for n in range(N+1)] for u in nodes]
     H = [[0 for n in range(N+1)] for u in nodes]
@@ -120,27 +138,36 @@ def fillMatrices(tree,N):
             i = u.getID()
             for m in range(1,n):
                     D[i][n] += H[i][n-m]*H[i][m]*Dup(u)
-            for v in u.getIncomparable():
-                j = v.getID()
-                for m in range(1,n):
-                    T[i][n] += H[i][n-m]*H[j][m]*Tran(u,v)
+            if ALLOW_TRANSFERS:
+                receivers = u.getIncomparable()
+                if u.getTime() >= 0:
+                    receivers = u.getContemporary()
+                for v in receivers:
+                    j = v.getID()
+                    for m in range(1,n):
+                        T[i][n] += H[i][n-m]*H[j][m]*Tran(u,v)
             if u.isLeaf():
                 if n==1:
                     H[i][n] = Ext(u)
                 else: # => n>1
                     H[i][n] = D[i][n] + T[i][n]
             else: # => n ancestor
-                l,r = u.getLeft(),u.getRight()
-                lid,rid= l.getID(),r.getID()
-                
-                S[i][n] = Loss(l)*H[rid][n] + Loss(r)*H[lid][n]
-                for m in range(1,n):
-                    S[i][n] += H[lid][n-m]*H[rid][m]
+                if u.isBinary():
+                    l,r = u.getLeft(),u.getRight()
+                    lid,rid= l.getID(),r.getID()
+                    S[i][n] = Loss(l)*H[rid][n] + Loss(r)*H[lid][n]
+                    for m in range(1,n):
+                        S[i][n] += H[lid][n-m]*H[rid][m]
+                if u.isUnary():
+                    c = u.getChild()
+                    cid = c.getID()
+                    S[i][n] = H[cid][n]
                     
                 H[i][n] = D[i][n] + S[i][n] + T[i][n]
     return S,H,D,T
 
 def randGen(u,state="H",n=0,S=[],H=[],D=[],T=[]):
+    global ALLOW_TRANSFERS
     i = u.getID()
     if state == 'D':
         rand = random.random()*D[i][n]
@@ -150,12 +177,16 @@ def randGen(u,state="H",n=0,S=[],H=[],D=[],T=[]):
                 return [DupLbl(u)]+randGen(u,'H',n-m,S,H,D,T)+randGen(u,'H',m,S,H,D,T)
     if state=="T":
         rand = random.random()*T[i][n]
-        for v in u.getIncomparable():
-            j = v.getID()
-            for m in range(1,n):
-                rand -= H[i][n-m]*H[j][m]*Tran(u,v)
-                if rand<0:
-                    return [TranLbl(u,v)]+randGen(u,'H',n-m,S,H,D,T)+randGen(v,'H',m,S,H,D,T)
+        if ALLOW_TRANSFERS:
+            receivers = u.getIncomparable()
+            if u.getTime() >= 0:
+                receivers = u.getContemporary()
+            for v in receivers:
+                j = v.getID()
+                for m in range(1,n):
+                    rand -= H[i][n-m]*H[j][m]*Tran(u,v)
+                    if rand<0:
+                        return [TranLbl(u,v)]+randGen(u,'H',n-m,S,H,D,T)+randGen(v,'H',m,S,H,D,T)
     if u.isLeaf():
         if state == 'H':
             if n==1:
@@ -169,20 +200,29 @@ def randGen(u,state="H",n=0,S=[],H=[],D=[],T=[]):
                 if rand<0:
                     return randGen(u,'T',n,S,H,D,T)
     else:
-        l,r = u.getLeft(),u.getRight()
-        lid,rid= l.getID(),r.getID()
         if state == 'S':
-            rand = random.random()*S[i][n]
-            rand -= Loss(l)*H[rid][n]
-            if rand<0:
-                return [LossLbl(l)] + randGen(r,'H',n,S,H,D,T)
-            rand -= Loss(r)*H[lid][n]
-            if rand<0:
-                return randGen(l,'H',n,S,H,D,T) + [LossLbl(r)]
-            for m in range(1,n):
-                rand -= H[lid][n-m]*H[rid][m]
+            if u.isBinary():
+                l,r = u.getLeft(),u.getRight()
+                lid,rid= l.getID(),r.getID()
+                rand = random.random()*S[i][n]
+                rand -= Loss(l)*H[rid][n]
                 if rand<0:
-                    return randGen(l,'H',n-m,S,H,D,T) + randGen(r,'H',m,S,H,D,T)
+                    return [LossLbl(l)] + randGen(r,'H',n,S,H,D,T)
+                rand -= Loss(r)*H[lid][n]
+                if rand<0:
+                    return randGen(l,'H',n,S,H,D,T) + [LossLbl(r)]
+                for m in range(1,n):
+                    rand -= H[lid][n-m]*H[rid][m]
+                    if rand<0:
+                        return randGen(l,'H',n-m,S,H,D,T) + randGen(r,'H',m,S,H,D,T)
+            if u.isUnary():
+                c = u.getChild()
+                cid = c.getID()
+                rand = random.random()*S[i][n]
+                rand -= H[cid][n]
+                if rand<0:
+                    return randGen(c,'H',n,S,H,D,T)
+                
         elif state == 'H':
             rand = random.random()*H[i][n]
             rand -= D[i][n]
@@ -202,6 +242,18 @@ def buildCaterpillar(k):
     else:
         return Node([Node(),buildCaterpillar(k-1)])
 
+def buildTimedCaterpillar(k,h=None):
+    if h is None:
+        h=0
+    if k==0:
+        return Node([],-1,h)
+    else:
+        # Single branch
+        u = Node([],-1,h+k)
+        for i in range(h+k-1,h,-1):
+            u = Node([u],-1,i)
+        return Node([u,buildTimedCaterpillar(k-1,h+1)],-1,h)
+
 def labelTree(t,currentID=0):
     for c in t.getChildren():
         currentID = labelTree(c,currentID)
@@ -209,27 +261,69 @@ def labelTree(t,currentID=0):
     return currentID+1
 
 def printTree(t,indent=0):
-    print " "*(indent)+"-> %s"%(t.getID())
+    print " "*(indent)+"-> %s (t=%s)"%(t.getID(),t.getTime())
     for c in t.getChildren():
         printTree(c,indent+1)
 
     
 PATH_GGD = "grammar.ggd"
 PATH_TMP = "out.txt"
+ALLOW_TRANSFERS = True
+
+def parseBoolean(s):
+    if s.lower()=="on" or s=="1":
+        return True
+    return False
+
 
 if __name__=="__main__":
     k = int(sys.argv[1])
     n = int(sys.argv[2])
-    nb = int(sys.argv[3])
+    nb = 0
+    timed = True
+    showHistories= True
+    showTree = False
+    showStats = False
+    showCount = True
 
-    cat = buildCaterpillar(k)
+    i = 3
+    while i<len(sys.argv):
+        if sys.argv[i].lower()=="--timed":
+            i+=1
+            timed = parseBoolean(sys.argv[i])
+        elif  sys.argv[i].lower()=="--showhistories":
+            i+=1
+            showHistories = parseBoolean(sys.argv[i])
+        elif  sys.argv[i].lower()=="--showtree":
+            i+=1
+            showTree = parseBoolean(sys.argv[i])
+        elif  sys.argv[i].lower()=="--showstats":
+            i+=1
+            showStats = parseBoolean(sys.argv[i])
+        elif  sys.argv[i].lower()=="--showcount":
+            i+=1
+            showCount = parseBoolean(sys.argv[i])
+        elif  sys.argv[i].lower()=="--sample":
+            i+=1
+            nb = int(sys.argv[i])
+        elif  sys.argv[i].lower()=="--notransfers":
+            ALLOW_TRANSFERS = False
+        i+=1
+
+    #cat = buildCaterpillar(k)
+    if timed:
+        cat = buildTimedCaterpillar(k)
+    else:
+        cat = buildCaterpillar(k)
+    
     labelTree(cat)
-    printTree(cat)
-
-##    for c in cat.allNodes():
-##        print c.getID(),[v.getID() for v in c.getIncomparable()]
+    if showTree:
+        printTree(cat)
 
     S,H,D,T = fillMatrices(cat,n)
+
+    if showCount:
+        print "#Histories: %s"%(int(H[cat.getID()][n]))
 
     raw = []
     allkeys = set()
@@ -244,44 +338,25 @@ if __name__=="__main__":
             vals[k] += 1
         allkeys = allkeys | set(vals.keys())
         raw.append(vals)
-    
-    
-##    ggd = createGGDGrammar(k)
-##    f = open(PATH_GGD,"w")
-##    f.write(ggd)
-##    f.close()
-##
-##    cmd = "java  -cp GenRGenS2.1-bin.jar GenRGenS.GenRGenS -size %s grammar.ggd -nb %s > %s"%(n,nb,PATH_TMP)
-##    print cmd
-##    os.system(cmd)
-##    
-##    for l in open(PATH_TMP):
-##        data = l.strip().split()
-##        vals = {}
-##        for k in data:
-##            if k not in vals:
-##                vals[k] = 0
-##            vals[k] += 1
-##        allkeys = allkeys | set(vals.keys())
-##        raw.append(vals)
+    if showStats:
+        nbEmpty = 0
+        for v in raw:
+            if showHistories:
+                print ",".join(v)
+            for k in allkeys:
+                if k not in v:
+                    nbEmpty +=1
+                    break
 
-    nbEmpty = 0
-    for v in raw:
-        for k in allkeys:
-            if k not in v:
-                nbEmpty +=1
-                break
-
-    vEmpty = {k:0 for k in allkeys}
-    vAvg = {k:0 for k in allkeys}
-    for v in raw:
-        for k in allkeys:
-            if k not in v:
-                vEmpty[k]+=1
-            else:
-                vAvg[k] += v[k]
-    
-    print len(raw), nbEmpty
-    print "   ".join(["%s->%.2f%% (%.2f)"%(k,100.*v/float(len(raw)),vAvg[k]/float(len(raw))) for (v,k) in sorted([(vEmpty[k],k) for k in allkeys])])
-
+        vEmpty = {k:0 for k in allkeys}
+        vAvg = {k:0 for k in allkeys}
+        for v in raw:
+            for k in allkeys:
+                if k not in v:
+                    vEmpty[k]+=1
+                else:
+                    vAvg[k] += v[k]
         
+        print "   ".join(["%s->%.2f%% (%.2f)"%(k,100.*v/float(len(raw)),vAvg[k]/float(len(raw))) for (v,k) in sorted([(vEmpty[k],k) for k in allkeys])])
+
+            
