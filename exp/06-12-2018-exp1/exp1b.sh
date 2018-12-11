aa62e68396ebcbde41962054514987f3baaba6cc
Cedric Chauve <chauvec@cedar5.cedar.computecanada.ca>
2018-12-11 11:54:28 -0800

    main scripts exp1a and exp1b

--- exp/06-12-2018-exp1/exp1b.sh
+++ exp/06-12-2018-exp1/exp1b.sh
@@ -6,7 +6,7 @@
 #SBATCH --error  /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/06-12-2018-exp1/traces/exp1a.error
 
 cd src
-python2.7  main_exp1a.py 33 64 100 128 20 ../results/exp1b
+python2.7  main_exp1.py 33 64 100 128 20 ../results/exp1b
 cd ../results
 gzip exp1b_*
 cd ..
