291b99d4437a86a9cb1a8b55ef8abce714e3f3b2
Cedric Chauve <chauvec@cedar5.cedar.computecanada.ca>
2018-12-11 11:35:46 -0800

    experiment 1b

---
+++ exp/06-12-2018-exp1/exp1b.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --mem=8000M
+#SBATCH --account=rrg-chauvec
+#SBATCH --output /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/06-12-2018-exp1/traces/exp1a.trace
+#SBATCH --error  /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/06-12-2018-exp1/traces/exp1a.error
+
+cd src
+python2.7  main_exp1a.py 33 64 100 128 20 ../results/exp1b
+cd ../results
+gzip exp1b_*
+cd ..
