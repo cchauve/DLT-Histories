fabd7e787c0d6b14f10a40c5b56ae85a97aa31ff
Cedric Chauve <chauvec@cedar5.cedar.computecanada.ca>
2019-02-06 06:00:31 -0800

    k=2

--- exp/05-02-19-k2/main_caterpillar.py
+++ exp/05-02-19-k2/main_caterpillar.py
@@ -1,4 +1,4 @@
-# We look at counting results for a species tree of size 2
+# We look at counting results for a caterpillar species tree
 
 from trees   import *
 from dpcount import *
@@ -11,20 +11,17 @@ def growth_constant(a,n):
     return(math.exp(math.log(math.pow(n,1.5)*a)/n))
 
 if __name__=="__main__":
-    k = 2
-    n = int(sys.argv[1])
-    m = int(sys.argv[2]) # Number of samples
+    k = int(sys.argv[1])
+    n = int(sys.argv[2])
     s = int(sys.argv[3])
-    tosample = (sys.argv[4] == 'True')
-    toprint  = (sys.argv[5] == 'True')
     
     random.seed(s)
 
-    utree  = buildCompleteTree(1)
+    utree  = buildCaterpillar(k)
     labelTree(utree)
     print(utree.asNewick())
     (ranking,rtree) = rankTreeRandomly(utree)
-    labelTree(utree)
+    labelTree(rtree)
     print(ranking)
     print(rtree.asNewick())
     uS1,uH1,uD1,uT1 = fillMatrices(utree,n,False)
@@ -35,15 +32,3 @@ if __name__=="__main__":
     print('U,DLT:\tn='+str(n)+'\t'+str(uH2[utree.getID()][n])+'\tgrowth constant='+str(growth_constant(uH2[utree.getID()][n],n)))
     print('R,DL:\tn='+str(n)+'\t'+str(rH1[rtree.getID()][n])+'\tgrowth constant='+str(growth_constant(rH1[rtree.getID()][n],n)))
     print('R,DLT:\tn='+str(n)+'\t'+str(rH2[rtree.getID()][n])+'\tgrowth constant='+str(growth_constant(rH2[rtree.getID()][n],n)))
-
-    if tosample:
-        print('Sampling u,DL histories')
-        histories_UDL = []
-        for i in range(0,m):
-            history = randGen(utree,"H",n,uS1,uH1,uD1,uT1)
-            if history not in histories_UDL:
-                histories_UDL.append(history)
-        print('There are '+str(len(histories_UDL))+' histories')
-        if toprint:
-            for h in histories_UDL:
-                print(h)
