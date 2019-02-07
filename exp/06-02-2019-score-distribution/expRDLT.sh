c27718c72170e1ee1e5495fcfb43b917447c6991
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2019-02-07 13:48:04 -0800

    sampling results

---
+++ exp/06-02-2019-score-distribution/expRDLT.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --time=48:00:00
+#SBATCH --mem=8000M
+#SBATCH --account=rrg-chauvec
+#SBATCH --output /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/06-02-2019-score-distribution/traces/expRDLT.trace
+#SBATCH --error  /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/06-02-2019-score-distribution/traces/expRDLT.error
+
+cd src
+python2.7 main.py 8 10 50 10000 True True ../results/samples_8_10_R_DLT
+python2.7 main.py 8 15 50 10000 True True ../results/samples_8_15_R_DLT
+python2.7 main.py 8 20 50 10000 True True ../results/samples_8_20_R_DLT
+python2.7 main.py 16 20 50 10000 True True ../results/samples_16_20_R_DLT
+python2.7 main.py 16 30 50 10000 True True ../results/samples_16_30_R_DLT
+cd ../results
+gzip samples_8_10_R_DLT
+gzip samples_8_15_R_DLT
+gzip samples_8_20_R_DLT
+gzip samples_16_20_R_DLT
+gzip samples_16_30_R_DLT
+cd ../
