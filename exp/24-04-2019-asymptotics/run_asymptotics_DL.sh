965a59f7170cfb889a389c4300384efd50779a49
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2019-04-25 03:51:52 -0700

    asymptotics DL

--- exp/24-04-2019-asymptotics/run_asymptotics_DL.sh
+++ exp/24-04-2019-asymptotics/run_asymptotics_DL.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --time=48:00:00
+#SBATCH --mem=20G
 #SBATCH --account=rrg-chauvec
-#SBATCH --output /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/15-04-2019-asymptotics/traces/main_DL.trace
-#SBATCH --error  /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/15-04-2019-asymptotics/traces/main_DL.error
+#SBATCH --output /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/24-04-2019-asymptotics/traces/main_DL.trace
+#SBATCH --error  /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/24-04-2019-asymptotics/traces/main_DL.error
 
-cd /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/15-04-2019-asymptotics/src
-for K in {3..16}
+cd /home/chauvec/wg-anoph/COUNTING_TREES/DLT-Histories/exp/24-04-2019-asymptotics/src
+for K in {3..25}
 do
     echo ${K}
-    python2.7 main_DL.py ../data/exp1a_${K}_add1_asy DL ../results/exp1a_${K}_DL_asy
+    python2.7 main_DL.py ../data/trees_${K} DL ../results/asymptotics_${K}_DL
 done
