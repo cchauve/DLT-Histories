6334e9013deab936b1050bf82e9f597cf35062bf
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2019-11-20 05:01:00 -0800

    code factorisation

--- exp/20-11-2019-Reconciliations/src/DLTcount.py
+++ exp/20-11-2019-Reconciliations/src/DLTcount.py
@@ -11,8 +11,11 @@ def Ext(u): # Extant leaf
     return(1.0)
 def Loss(u): # Gene loss
     return(1.0)
-def Dup(u): # Gene duplication
-    return(1.0)
+HIST='H'
+REC='R'
+def Dup(u,HR): # Gene duplication, HR = 'H' for history, 'R' for reconciliation
+    COEFF={HIST:1.0,REC:0.5}
+    return(COEFF[HR])
 def HGT(u,v): # Horizontal Gene Transfer
     return(1.0)
 # Functions to print evolutionary events
@@ -26,15 +29,16 @@ def HGTLbl(u,v):
     return("T%s->%s"%(u.getID(),v.getID()))
 
 # ---------------------------------------------------------------------------
-# Counting histories
+# Counting histories and reconciliations
 
 # Filling the DP matrix counting the number of histories per size and per subtree
 # X: parameter aimed at skewing the uniform distribution for sampling
 # X < 1: parsimonious histories are more likely to be sampled
 # X > 1: non-parsimonious histories are more likely to be sampled
 # The same value of X should be used in filleMatrices and randGen
-# MODEL specifies the evolutionary model
-def fillMatricesHist(tree,N,MODEL={'D':True,'L':True,'T':False},X=1.0):
+# E_MODEL specifies the evolutionary model
+# O_MODEL specifies the object (history or reconciliation), HIST or REC
+def fillMatrices_aux(tree,N,E_MODEL={'D':True,'L':True,'T':False},O_MODEL=HIST,X=1.0):
 
     nodes = tree.allNodes()
 
@@ -46,10 +50,12 @@ def fillMatricesHist(tree,N,MODEL={'D':True,'L':True,'T':False},X=1.0):
     for n in range(1,N+1):
         for u in nodes:
             i = u.getID()
-            if MODEL['D']:
+            if E_MODEL['D']:
                 for m in range(1,n):
-                    D[i][n] += H[i][n-m]*H[i][m]*Dup(u)*X
-            if MODEL['T']:
+                    D[i][n] += H[i][n-m]*H[i][m]*Dup(u,O_MODEL)*X
+                if n%2==0:
+                    D[i][n] += H[i][int(n/2)]*(1.0-Dup(u,O_MODEL))*X
+            if E_MODEL['T']:
                 if u.getTime() >= 0:
                     receivers = u.getContemporary()
                 else:
@@ -67,7 +73,7 @@ def fillMatricesHist(tree,N,MODEL={'D':True,'L':True,'T':False},X=1.0):
                 if u.isBinary():
                     l,r = u.getLeft(),u.getRight()
                     lid,rid= l.getID(),r.getID()
-                    if MODEL['L']:
+                    if E_MODEL['L']:
                         S[i][n] = X*(Loss(l)*H[rid][n] + Loss(r)*H[lid][n])
                     for m in range(1,n):
                         S[i][n] += H[lid][n-m]*H[rid][m]
@@ -80,57 +86,12 @@ def fillMatricesHist(tree,N,MODEL={'D':True,'L':True,'T':False},X=1.0):
 
     return(S,H,D,T)
 
-# ---------------------------------------------------------------------------
-# Counting reconciliations
+def fillMatricesHist(tree,N,E_MODEL,X):
+    def fillMatrices_aux(tree,N,E_MODEL,HIST,X)
 
-def fillMatricesRec(tree,N,MODEL={'D':True,'L':True,'T':False},X=1.0):
-
-    nodes = tree.allNodes()
-
-    S = [[0 for n in range(N+1)] for u in nodes]
-    H = [[0 for n in range(N+1)] for u in nodes]
-    D = [[0 for n in range(N+1)] for u in nodes]
-    T = [[0 for n in range(N+1)] for u in nodes]
-
-    for n in range(1,N+1):
-        for u in nodes:
-            i = u.getID()
-            if MODEL['D']:
-                for m in range(1,n):
-                    D[i][n] += 0.5*H[i][n-m]*H[i][m]*Dup(u)*X
-                    if n%2==0:
-                        D[i][n] += 0.5*H[i][int(n/2)]*Dup(u)*X
-            if MODEL['T']:
-                if u.getTime() >= 0:
-                    receivers = u.getContemporary()
-                else:
-                    receivers = u.getIncomparable()
-                for v in receivers:
-                    j = v.getID()
-                    for m in range(1,n):
-                        T[i][n] += H[i][n-m]*H[j][m]*HGT(u,v)*X
-            if u.isLeaf():
-                if n==1:
-                    H[i][n] = Ext(u)
-                else: # => n>1
-                    H[i][n] = D[i][n] + T[i][n]
-            else: # => n ancestor
-                if u.isBinary():
-                    l,r = u.getLeft(),u.getRight()
-                    lid,rid= l.getID(),r.getID()
-                    if MODEL['L']:
-                        S[i][n] = X*(Loss(l)*H[rid][n] + Loss(r)*H[lid][n])
-                    for m in range(1,n):
-                        S[i][n] += H[lid][n-m]*H[rid][m]
-                if u.isUnary():
-                    c   = u.getChild()
-                    cid = c.getID()
-                    S[i][n] = H[cid][n]
-                    
-                H[i][n] = D[i][n] + S[i][n] + T[i][n]
-
-    return(S,H,D,T)
-
+def fillMatricesRec(tree,N,E_MODEL,X):
+    def fillMatrices_aux(tree,N,E_MODEL,REC,X)
+        
 # ---------------------------------------------------------------------------
 
 USAGE = 'DLTcount <tree> <MODEL> <n>\n'+'tree = random k | rrandom k | caterpillar k | rcaterpillar k | complete h | rcomplete h | newick string\n'+'\trandom k = random binary tree with k leaves\n'+'\trrandom k = randomly ranked random binary tree with k leaves\n'+'\tcaterpillar k = caterpillar with k leaves\n'+'\trcaterpillar k = randomly ranked caterpillar with k leaves\n'+'\tcomplete h = complete binary tree with 2^h leaves\n'+'\trcomplete h = randomly ranked complete binary tree with 2^h leaves\n'+'\tnewick string = string is the Newick representation of a tree\n'+'MODEL = DL | DLT\n'+'n = history size\n'
